333da47d48498bccde5f13644ee8b87ae85fa0cc
basvancleef1 <basvancleef@icloud.com>
2024-06-09 19:05:51 +0200

    feat: Implement GlobalUsings for imports

---
+++ CryptoTracker/GlobalUsings.cs
@@ -0,0 +1,8 @@
+global using CryptoTracker.Models;
+global using CryptoTracker.Views;
+global using CryptoTracker.ViewModels;
+global using CryptoTracker.Services;
+global using CommunityToolkit.Mvvm.ComponentModel;
+global using System.Collections.ObjectModel;
+global using System.Diagnostics;
+global using System.Text.Json;
